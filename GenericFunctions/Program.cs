2b562b8861143ec9462f7ce5987f6789bec0f5df
Ruper Ottolenghi <ruper120406@gmail.com>
2023-01-09 13:27:51 +0200

    Add project files.

---
+++ GenericFunctions/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericFunctions
+{
+    internal class Program
+    {
+        static dynamic InputLine()
+        {
+            dynamic x = null;
+
+            Console.WriteLine("pls enter the variable value");
+
+            try
+            {
+                x = Console.ReadLine();
+
+                try
+                {
+                    x = Convert.ToInt64(x);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            catch (FormatException e)
+            {
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return x;
+        }
+        static dynamic genericFunction(dynamic x, dynamic y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            dynamic x, y;
+            x = InputLine();
+
+            y = InputLine();
+
+            Console.WriteLine($"{x} + {y} = {genericFunction(x, y)}");
+        }
+    }
+}
